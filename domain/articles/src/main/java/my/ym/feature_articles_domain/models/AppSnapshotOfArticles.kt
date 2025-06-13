cf01abe2394050390702cc6ed4a7628bc6aaee58
Mohamed Alaa <36743366+MohamedAlaaEldin636@users.noreply.github.com>
2025-06-14 01:54:12 +0300

    Added tests to :domain:articles el7mdullah & inshallah (#4)

--- domain/articles/src/main/java/my/ym/feature_articles_domain/models/AppSnapshotOfArticles.kt
+++
@@ -1,9 +0,0 @@
-package my.ym.feature_articles_domain.models
-
-import java.time.LocalDateTime
-
-data class AppSnapshotOfArticles(
-	val fetchedFromApiAt: LocalDateTime,
-	val usedCache: Boolean,
-	val articles: List<AppViewedArticle>,
-)
